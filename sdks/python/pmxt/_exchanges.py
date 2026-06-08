a6190b904b752e80b05899dc16f45de9bb9b139f
Samuel Tinnerholm <samuel.tinnerholm@gmail.com>
2026-06-08 08:11:07 +0000

    chore: regenerate Python exchange exports

--- sdks/python/pmxt/_exchanges.py
+++ sdks/python/pmxt/_exchanges.py
@@ -547,5 +547,3 @@ class Router(Exchange):
 # Backwards-compatible aliases for exchange classes generated before underscore handling.
 Polymarket_us = PolymarketUS
 Suibets = SuiBets
-
-from .router import Router as Router
